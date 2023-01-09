b2fa58456940d82582011d9f4c830bb5dee4dcd4
Oluwadarasimi Odejobi <oluwadarasimi5000@gmail.com>
2023-01-09 20:43:21 +0100

    finished designing for desktop

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ document.getElementById("monthly").style.color = "#47587d";
 document.getElementById("yearly").style.color = "#bdbec4";
 document.getElementById("next-monthly").style.display = "block";
 document.getElementById("next-yearly").style.display = "none";
+document.getElementById("arcade-year-one").style.display = "none";
+document.getElementById("arcade-year-two").style.display = "none";
+document.getElementById("advanced-year-one").style.display = "none";
+document.getElementById("advanced-year-two").style.display = "none";
+document.getElementById("pro-year-one").style.display = "none";
+document.getElementById("pro-year-two").style.display = "none";
 
 toggle.addEventListener("change", function () {
   if (toggle.checked) {
@@ -11,15 +17,31 @@ toggle.addEventListener("change", function () {
     document.getElementById("yearly").style.color = "#47587d";
     document.getElementById("next-monthly").style.display = "none";
     document.getElementById("next-yearly").style.display = "block";
-    
+    document.getElementById("arcade-month").style.display = "none";
+    document.getElementById("advanced-month").style.display = "none";
+    document.getElementById("pro-month").style.display = "none";
+    document.getElementById("arcade-year-one").style.display = "block";
+    document.getElementById("arcade-year-two").style.display = "block";
+    document.getElementById("advanced-year-one").style.display = "block";
+    document.getElementById("advanced-year-two").style.display = "block";
+    document.getElementById("pro-year-one").style.display = "block";
+    document.getElementById("pro-year-two").style.display = "block";
   } else {
     document.getElementById("monthly").style.color = "#47587d";
     document.getElementById("yearly").style.color = "#bdbec4";
     document.getElementById("next-monthly").style.display = "block";
     document.getElementById("next-yearly").style.display = "none";
+    document.getElementById("arcade-month").style.display = "block";
+    document.getElementById("advanced-month").style.display = "block";
+    document.getElementById("pro-month").style.display = "block";
+    document.getElementById("arcade-year-one").style.display = "none";
+    document.getElementById("arcade-year-two").style.display = "none";
+    document.getElementById("advanced-year-one").style.display = "none";
+    document.getElementById("advanced-year-two").style.display = "none";
+    document.getElementById("pro-year-one").style.display = "none";
+    document.getElementById("pro-year-two").style.display = "none";
   }
 });
-
 
 var inputs = document.getElementsByTagName("input");
 
